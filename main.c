c95cee6a882e2bd17f95e65c647a14b671582493
f3r10 <frledesma@outlook.com>
2022-05-29 09:55:08 -0500

    improved runtime print function and general warnings tweaks

--- main.c
+++ main.c
@@ -1,83 +1,117 @@
+#include <errno.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
 #define TRUE 0xFFFFFFFF
 #define FALSE 0x7FFFFFFF
 
-#define BOA_MIN (- (1L << 62))
+#define BOA_MIN (-(1L << 62))
 #define BOA_MAX ((1L << 62) - 1)
 
-extern int64_t our_code_starts_here(int64_t input_val, int64_t heap_pointer) asm("our_code_starts_here");
+extern int64_t
+our_code_starts_here(int64_t input_val,
+                     int64_t heap_pointer) asm("our_code_starts_here");
 extern void error(int64_t val) asm("error");
 extern void error_non_number(int64_t val) asm("error_non_number");
 extern void error_index_out_of_bounds() asm("error_index_out_of_bounds");
-extern int64_t print(int64_t val, int printSpace) asm("print");
+extern void print(int64_t val, int printSpace) asm("print");
 
-int64_t isPointer(int64_t p) {
-  return (p & 0x00000007) == 0x00000001;
-}
+int64_t isPointer(int64_t p) { return (p & 0x00000007) == 0x00000001; }
 
-int64_t print(int64_t val, int printSpace) {
+void print(int64_t val, int printSpace) {
   if (val & 0x00000001 ^ 0x00000001) {
     if (BOA_MIN > val || val > BOA_MAX) {
       fprintf(stderr, "overflow");
       exit(1);
     } else {
       if (printSpace == 1) {
-        printf("\n%lld\n", val >> 1);
+        printf("%" PRId64 "\n", val >> 1);
       } else {
-        printf("%lld", val >> 1);
+        printf("%" PRId64, val >> 1);
       }
-      return val;
     }
   } else if (val == FALSE) {
-    printf("false\n");
-    return val;
+    if (printSpace == 1) {
+      printf("false\n");
+    } else {
+      printf("false");
+    }
   } else if (val == TRUE) {
-    printf("true\n");
-    return val;
-  } else if (val == 0)  {
+    if (printSpace == 1) {
+      printf("true\n");
+    } else {
+      printf("true");
+    }
+  } else if (val == -2) {
     printf("null\n");
   } else if (isPointer(val)) {
     /*
-      Tuple -> [type, val, pointer] -> [type, val, pointer]
-      Vec -> [type, num_elements, elem_1, elem_2, ..... elem_n]
+      Tuple -> [type:1, val, pointer] -> [type, val, pointer]
+      Vec -> [type:2, num_elements, elem_1, elem_2, ..... elem_n]
+      Dict -> [type:3, numElements, elem_1, elem_2, ..... elem_n]
     */
-    int64_t* valp = (int64_t*) (val - 1);    // extract address  
+    int64_t *valp = (int64_t *)(val - 1); // extract address
     // printf("valp: %lld\n", valp);
-    int64_t* structureType = (*valp);
-    if (structureType == 1) { // This is a tuple
+    int64_t *structureType = &(*valp);
+    if (*structureType == 1) { // This is a tuple
       printf("(");
-      print(*(valp + 1), 0);                 // print recursive first element
+      print(*(valp + 1), 0); // print recursive first element
       printf(",");
-      print(*(valp + 2), 0);           // print recursive second element
-      printf(")");
-    } else if (structureType == 2) { // This is a vector
-      int64_t* numElements = (*(valp + 1));
+      print(*(valp + 2), 0); // print recursive second element
+      if (printSpace == 1) {
+        printf(")\n");
+      } else {
+        printf(")");
+      }
+    } else if (*structureType == 2) { // This is a vector
+      int64_t *numElements = &(*(valp + 1));
       printf("(");
       int i;
-      for (i = 1; i <= numElements; i = i + 1) {
+      for (i = 1; i <= *numElements; i = i + 1) {
         if (i != 1) {
           printf(",");
         }
-        printf("%lld", *(valp + 1 + i) >> 1);
+        printf("%ld", *(valp + 1 + i) >> 1);
       }
-      printf(")");
+      if (printSpace == 1) {
+        printf(")\n");
+      } else {
+        printf(")");
+      }
+    } else if (*structureType == 3) {
+      int64_t *numElements = &(*(valp + 1));
+      printf("{");
+      int i;
+      for (i = 1; i <= *numElements; i = i + 1) {
+        if (i != 1) {
+          printf(", ");
+        }
+        printf("_%i: ", i);
+        print(*(valp + 1 + i), 0);
+      }
+      if (printSpace == 1) {
+        printf("}\n");
+      } else {
+        printf("}");
+      }
     } else {
       printf("Unknown structure\n");
     }
 
-    // return val;
   } else {
-    fprintf(stderr, "Unknown value: %lld\n", val);
-    exit(1);
+    if (val == -1) {
+      // printf("\n");
+    } else {
+      fprintf(stderr, "Unknown value: %ld\n", val);
+      exit(1);
+    }
   }
 }
 
 void error(int64_t error_code) {
-  fprintf(stderr, "Error: %lld\n", error_code);
+  fprintf(stderr, "Error: %" PRId64 "\n", error_code);
   exit(1);
 }
 
@@ -91,7 +125,7 @@ void error_index_out_of_bounds() {
   exit(1);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   // int64_t result = our_code_starts_here();
   // printf("\nThe rep was: %lld", result);
   int64_t input_val;
@@ -111,13 +145,12 @@ int main(int argc, char** argv) {
       fprintf(stderr, "input is not a representable number\n");
       exit(1);
     } else {
-      input_val = (int64_t) v;
+      input_val = (int64_t)v;
       input_val = (input_val << 1);
     }
   }
 
-
-  int64_t* HEAP = malloc(8 * 100000);
+  int64_t *HEAP = malloc(8 * 100000);
   int64_t result = our_code_starts_here(input_val, HEAP);
   print(result, 1);
   free(HEAP);
